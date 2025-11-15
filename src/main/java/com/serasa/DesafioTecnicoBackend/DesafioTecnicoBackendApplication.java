aa53f17add4e37eb2b8254f7d7901f5e1b57386c
Everton Miyabukuro <evertonmiyabukuro@gmail.com>
2025-11-15 15:47:17 -0300

    Repositório inicializado com o Spring Initializer

---
+++ src/main/java/com/serasa/DesafioTecnicoBackend/DesafioTecnicoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.serasa.DesafioTecnicoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serasa/DesafioTecnicoBackend/DesafioTecnicoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serasa.DesafioTecnicoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
